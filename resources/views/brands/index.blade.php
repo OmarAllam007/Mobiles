90b05cffc21466b309b5effee58cd036aa712752
omar <omar.coder007@gmail.com>
2018-09-11 12:15:51 +0300

    brands style

--- resources/views/brands/index.blade.php
+++ resources/views/brands/index.blade.php
@@ -12,31 +12,32 @@
 @stop
 
 @section('body')
-    <section class="col-12">
+    <div class="row" >
         @if ($brands->count())
             @foreach($brands as $brand)
-                <div class="card" style="width: 18rem;">
-                    <img class="card-img-top" src="{{asset('storage'.$brand->image_path)}}">
-                    <div class="card-body">
-                        <h5 class="card-title">{{$brand->name}}</h5>
-                        <form action="{{route('brand.destroy', $brand)}}" method="post">
-                            <a href="{{route('brand.edit', $brand)}}" class="btn btn-primary">
-                            <i class="fa fa-edit"></i>
-                                {{t('Edit')}}
-                            </a>
-                            {{csrf_field()}} {{method_field('delete')}}
-                            <button class="btn  btn-danger"><i class="fa fa-trash"></i>
-                                {{t('Delete')}}
-                            </button>
-                        </form>
+                <div class="col-3" style="margin-top: 10px">
+                    <div class="card">
+                        <img class="card-img-top" src="{{asset('storage'.$brand->image_path)}}">
+                        <div class="card-body">
+                            <h5 class="card-title">{{$brand->name}}</h5>
+                            <form action="{{route('brand.destroy', $brand)}}" method="post">
+                                <a href="{{route('brand.edit', $brand)}}" class="btn btn-primary">
+                                    <i class="fa fa-edit"></i>
+                                    {{t('Edit')}}
+                                </a>
+                                {{csrf_field()}} {{method_field('delete')}}
+                                <button class="btn  btn-danger"><i class="fa fa-trash"></i>
+                                    {{t('Delete')}}
+                                </button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             @endforeach
-
         @else
             <div class="alert alert-info"><i class="fa fa-exclamation-circle"></i>
                 <strong>{{t('No Brands found')}}</strong>
             </div>
         @endif
-    </section>
+    </div>
 @stop
