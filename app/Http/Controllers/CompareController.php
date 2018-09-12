cebdc037482f4ccc703c7dd9d124b743227881bd
omar <omar.coder007@gmail.com>
2018-09-12 11:51:05 +0300

    main text

--- app/Http/Controllers/CompareController.php
+++ app/Http/Controllers/CompareController.php
@@ -10,18 +10,19 @@ class CompareController extends Controller
     function getCompare()
     {
         $mobiles = Mobile::all()->each(function ($mobile) {
-            $mobile['image'] = $mobile->image_path ?  asset('storage' . $mobile->image_path): asset('storage/no-phone.png');
+            $mobile['image'] = $mobile->image_path ? asset('storage' . $mobile->image_path) : asset('storage/no-phone.png');
         });
         return view('compare.compare', compact('mobiles'));
     }
 
-    function analyzeCompareProcess()
+    function analyzeCompareProcess(Request $request)
     {
         $new_mobiles = [];
-        $mobiles = Mobile::whereIn('id', \request('mobiles'))->get();
+        return $request->get('mobiles');
+        $mobiles = Mobile::whereIn('id', $request->get('mobiles'))->get();
         foreach ($mobiles as $key => $mobile) {
             $mobiles[$key] = $mobile;
-            $mobiles[$key]['image'] = $mobile->image_path ?  asset('storage' . $mobile->image_path): asset('storage/no-phone.png');
+            $mobiles[$key]['image'] = $mobile->image_path ? asset('storage' . $mobile->image_path) : asset('storage/no-phone.png');
             $mobiles[$key]['others_open_device'] = $this->getOpenBy($mobile);
             $mobiles[$key]['communication_network'] = $this->getNetwork($mobile);
         }
@@ -30,24 +31,29 @@ class CompareController extends Controller
 
     function getOpenBy($mobile)
     {
-        $openBy = [];
+
         if (count($mobile->others_open_device)) {
+            $openBy = [];
             foreach ($mobile->others_open_device as $key => $open) {
                 $openBy[$key] = Mobile::$openBy[$open];
             }
+            return $openBy;
         }
-        return $openBy;
-
+        return '';
     }
 
     function getNetwork($mobile)
     {
-        $network = [];
         if (count($mobile->communication_network)) {
+            $network = [];
+
             foreach ($mobile->communication_network as $key => $cn) {
                 $network[$key] = Mobile::$network[$cn];
             }
+            return $network;
         }
-        return $network;
+
+        return '';
+
     }
 }
