dad6dffdfd95d5eb79befcf528275e5c6a78b5fb
omar <omar.coder007@gmail.com>
2018-09-12 14:02:22 +0300

    styles_of_compaer

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -129,7 +129,6 @@
 
 <script type="text/javascript" src="{{asset('js/app.js')}}"></script>
 @yield('scripts')
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
 <script type="text/javascript">
     $(function () {
