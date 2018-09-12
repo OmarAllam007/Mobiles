ca44aaf7163b04a92b136297d57bfa9a04519a50
omar <omar.coder007@gmail.com>
2018-09-12 12:36:29 +0300

    styles_of_compare

--- app/Http/Controllers/CompareController.php
+++ app/Http/Controllers/CompareController.php
@@ -18,7 +18,7 @@ class CompareController extends Controller
     function analyzeCompareProcess(Request $request)
     {
         $new_mobiles = [];
-        return $request->query('mobiles');
+        return [$request->query('mobiles'),$request->get('mobiles'),\request('mobiles')];
         return $request->get('mobiles');
         $mobiles = Mobile::whereIn('id', $request->get('mobiles'))->get();
         foreach ($mobiles as $key => $mobile) {
