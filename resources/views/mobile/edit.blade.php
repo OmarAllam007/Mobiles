2d6558e74d106a3cc05b5b5de90c4f7192477a97
omar <omar.coder007@gmail.com>
2018-09-13 17:12:47 +0300

    edit-return

--- resources/views/mobile/edit.blade.php
+++ resources/views/mobile/edit.blade.php
@@ -8,7 +8,7 @@
         <a href="{{route('mobile.index')}}" class="btn btn-outline-info"><i class="fa fa-chevron-left"></i>
         </a>
     </div>
-@stop
+@endsection
 
 
 @section('body')
@@ -16,4 +16,60 @@
             {{csrf_field()}} {{method_field('patch')}}
             @include('mobile._form')
         </form>
-@stop
+@endsection
+
+@section('scripts')
+    <script>
+
+        $(document).ready(function () {
+            window.history.pushState(null, "", window.location.href);
+
+            window.onbeforeunload = function (e) {
+                return confirm('asdsd')
+            }
+            window.onpopstate = function () {
+                Confirm('Alert', 'Are you sure you want exit this page ?', 'Yes', 'Cancel', 'Blabla');
+            };
+
+
+
+            function Confirm(title, msg, $true, $false, $link) { /*change*/
+                var $content = "<div class='dialog-ovelay'>" +
+                    "<div class='dialog'><header>" +
+                    " <h3> " + title + " </h3> " +
+                    "<i class='fa fa-close'></i>" +
+                    "</header>" +
+                    "<div class='dialog-msg'>" +
+                    " <p> " + msg + " </p> " +
+                    "</div>" +
+                    "<footer id='footer'>" +
+                    "<div class='controls'>" +
+                    " <button class='button button-danger doAction'>" + $true + "</button> " +
+                    " <button class='button button-default cancelAction'>" + $false + "</button> " +
+                    "</div>" +
+                    "</footer>" +
+                    "</div>" +
+                    "</div>";
+
+                $('body').prepend($content);
+                $('.doAction').click(function () {
+                    window.history.back();
+                    $(this).parents('.dialog-ovelay').fadeOut(500, function () {
+                        $(this).remove();
+                    });
+                });
+                $('.cancelAction, .fa-close').click(function () {
+                    window.history.pushState(null,"href", window.location.href);
+
+                    $(this).parents('.dialog-ovelay').fadeOut(500, function () {
+                        $(this).remove();
+                    });
+                });
+
+            }
+
+
+        });
+
+    </script>
+@append
