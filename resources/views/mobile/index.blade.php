347bd49cfcf73700fb80165b64ac639b721ad2b6
omar <omar.coder007@gmail.com>
2018-09-18 01:18:26 +0300

    Favourite Module

--- resources/views/mobile/index.blade.php
+++ resources/views/mobile/index.blade.php
@@ -11,7 +11,21 @@
 @stop
 
 @section('body')
+    @php
+        $google = `<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <!-- mobile -->
+    <ins class="adsbygoogle"
+         style="display:block"
+         data-ad-client="ca-pub-1518734753701301"
+         data-ad-slot="9346035778"
+         data-ad-format="auto"
+         data-full-width-responsive="true"></ins>
+    <script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>`;
 
+        echo $google;
+    @endphp
     @if ($mobiles->count())
         <div class="row" style="margin-top: 10px" id="mobiles">
             @foreach($mobiles as $mobile)
