bcf2269edb6dee2166585d68283add7cc6010557
omar <omar.coder007@gmail.com>
2018-09-18 01:09:30 +0300

    Favourite Module

--- resources/views/mobile/show.blade.php
+++ resources/views/mobile/show.blade.php
@@ -6,9 +6,8 @@
 
 
 @section('body')
-    <amp-auto-ads type="adsense"
-                  data-ad-client="ca-pub-1518734753701301">
-    </amp-auto-ads>
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> <!-- MobArrow-responsive --> <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-1864972736029501" data-ad-slot="7077188983" data-ad-format="auto" data-full-width-responsive="true"></ins> <script> (adsbygoogle = window.adsbygoogle || []).push({}); </script>
+
 
     @php
             $is_favourite = 0;
