bfbb31f682ad092907b068cc78050799bee99ecd
omar <omar.coder007@gmail.com>
2018-09-18 01:21:33 +0300

    Favourite Module

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -7,7 +7,17 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{env('APP_NAME')}}</title>
-
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <!-- mobile -->
+    <ins class="adsbygoogle"
+         style="display:block"
+         data-ad-client="ca-pub-1518734753701301"
+         data-ad-slot="9346035778"
+         data-ad-format="auto"
+         data-full-width-responsive="true"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
     <!-- Fonts -->
     {{--<link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">--}}
 
