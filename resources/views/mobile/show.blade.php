66cb2fa3be80f45e9efe16a04e9a5ed9a48ea727
omar <omar.coder007@gmail.com>
2018-09-17 23:56:23 +0300

    likes

--- resources/views/mobile/show.blade.php
+++ resources/views/mobile/show.blade.php
@@ -128,8 +128,5 @@
 @endsection
 
 @section('scripts')
-    <script>
-
-
-    </script>
+    <script async="" src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> <!-- MobArrow-responsive --> <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-1864972736029501" data-ad-slot="7077188983" data-ad-format="auto" data-full-width-responsive="true"></ins> <script> (adsbygoogle = window.adsbygoogle || []).push({}); </script>
 @append
