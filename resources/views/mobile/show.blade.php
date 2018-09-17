a2d41db2b115281f41e18b76fc025bab3ea774f4
omar <omar.coder007@gmail.com>
2018-09-17 23:55:53 +0300

    likes

--- resources/views/mobile/show.blade.php
+++ resources/views/mobile/show.blade.php
@@ -17,7 +17,6 @@
             <div class="col-8">
                 @if(isset($mobile->advertisements[0]) && $mobile->advertisements[0])
                     <div class="card">
-                        <script async="" src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> <!-- MobArrow-responsive --> <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-1864972736029501" data-ad-slot="7077188983" data-ad-format="auto" data-full-width-responsive="true"></ins> <script> (adsbygoogle = window.adsbygoogle || []).push({}); </script>
                     </div>
                 @endif
 
