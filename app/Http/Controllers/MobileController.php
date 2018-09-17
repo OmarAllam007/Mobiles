4e6ef4c8808567849370e34bb138e07f7765037e
omar <omar.coder007@gmail.com>
2018-09-17 14:16:43 +0300

    likes

--- app/Http/Controllers/MobileController.php
+++ app/Http/Controllers/MobileController.php
@@ -133,12 +133,12 @@ class MobileController extends Controller
 
     public function makeFavourite(Request $request)
     {
-        return 2;
+
         if (\Auth::check()) {
             $mobile = Mobile::find($request->get('mobile_id'));
             $mobile->users()->toggle($mobile->id);
 //            return $mobile->users->count();
-            return response($mobile->users()->count(), 200)
+            return response(''.$mobile->users->count(), 200)
                 ->header('Content-Type', 'text/plain');
         }
     }
