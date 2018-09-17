d7dd56aa361239acc0c520a5956b859faa85f6a4
omar <omar.coder007@gmail.com>
2018-09-17 11:37:18 +0300

    likes

--- app/Http/Controllers/MobileController.php
+++ app/Http/Controllers/MobileController.php
@@ -130,10 +130,11 @@ class MobileController extends Controller
 
     function makeFavourite(Request $request)
     {
-        dump($request->get('mobile_id'));
+
         if (Auth::check()) {
             $is_favourite = Auth::user()->isFavourite($request->get('mobile_id'));
             $mobile = Mobile::find($request->get('mobile_id'));
+            dump($is_favourite,$mobile->id);
             $mobile->likes()->toggle($mobile->id);
             return response()->json(['count' => $mobile->likes->count(), 'is_favourite' => $is_favourite]);
 
