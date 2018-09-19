4be34977e373a7b15dbab49c22b65de199ab89ce
omar <omar.coder007@gmail.com>
2018-09-19 12:55:17 +0300

    likes

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -155,7 +155,19 @@
         <script>
             (adsbygoogle = window.adsbygoogle || []).push({});
         </script>
-        
+
+        <script async src=\"//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\"></script>
+<!-- UserAdsMobile -->
+<ins class=\"adsbygoogle\"
+     style=\"display:block\"
+     data-ad-client=\"ca-pub-1518734753701301\"
+     data-ad-slot=\"9249563350\"
+     data-ad-format=\"auto\"
+     data-full-width-responsive=\"true\"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
     </div>";
 
     echo $adsence;
