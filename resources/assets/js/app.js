4d3a609903791ca67de62cdac1475ed9e005e99d
omar <omar.coder007@gmail.com>
2018-10-24 20:28:29 +0300

    new style

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -14,13 +14,13 @@ window.Vue = require('vue');
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
-// import Ads from 'vue-google-adsense'
+import Ads from 'vue-google-adsense'
 //
-// Vue.use(require('vue-script2'))
+Vue.use(require('vue-script2'))
 //
-// Vue.use(Ads.Adsense)
-// Vue.use(Ads.InArticleAdsense)
-// Vue.use(Ads.InFeedAdsense)
+Vue.use(Ads.Adsense)
+Vue.use(Ads.InArticleAdsense)
+Vue.use(Ads.InFeedAdsense)
 
 var token = document.head.querySelector('meta[name="csrf-token"]');
 window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
@@ -33,7 +33,7 @@ Vue.component('choose-for-me', require('./components/ChooseForMeComponent.vue'))
 Vue.component('like-component', require('./components/LikeComponent.vue'));
 Vue.component('comments', require('./components/CommentsComponent.vue'));
 Vue.component('gallery-component', require('./components/GalleryComponent.vue'));
-// Vue.component('ad-component', require('./components/AdComponent.vue'));
+Vue.component('ad-component', require('./components/AdComponent.vue'));
 
 
 
