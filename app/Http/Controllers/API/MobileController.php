378aaed391931f1465ea9c34b533e23a79f01213
omar <omar.coder007@gmail.com>
2019-01-09 13:32:38 +0300

    redesign pricing

--- app/Http/Controllers/API/MobileController.php
+++ app/Http/Controllers/API/MobileController.php
@@ -17,13 +17,14 @@ class MobileController extends Controller
             $price = $request->price;
             $mobiles = Mobile::where('main_price_description', '>=', floatval($price))->count();
 
-            if($mobiles) {
+            if ($mobiles) {
                 return Mobile::whereNotNull('main_price_description')
+                    ->where('main_price_description', '>', 0)
                     ->get()
                     ->groupBy('brand.name')
-                    ->map(function ($items) use ($request,$price) {
+                    ->map(function ($items) use ($request, $price) {
                         return $items['mobiles'] = $items
-                            ->where('main_price_description', '<=', $price+200)
+                            ->where('main_price_description', '<=', $price + 200)
                             ->where('main_price_description', '>=', $price)
                             ->sortByDesc('main_price_description')
                             ->take(30);
@@ -32,9 +33,11 @@ class MobileController extends Controller
             }
 
         } else {
-            return Mobile::all()->groupBy('brand.name')->map(function ($items) {
-                return $items['mobiles'] = $items->sortByDesc('main_price_description')->take(30);
-            })->toArray();
+            return Mobile::all()
+                ->groupBy('brand.name')
+                ->map(function ($items) {
+                    return $items['mobiles'] = $items->sortByDesc('main_price_description')->take(30);
+                })->toArray();
         }
 
 
