c4684f2d731cf2fc79276202fa7a6795d9b621bb
omar <omar.coder007@gmail.com>
2019-01-16 12:48:54 +0300

    Search Component

--- public/js/app.js
+++ public/js/app.js
@@ -52389,7 +52389,7 @@ var render = function() {
                         [
                           _c("img", {
                             staticClass: "mobile-image",
-                            attrs: { src: mobile.image_path }
+                            attrs: { src: mobile.image }
                           }),
                           _vm._v(" "),
                           _c("div", { staticClass: "mobile-description" }, [
@@ -52423,7 +52423,7 @@ var render = function() {
                         [
                           _c("img", {
                             staticClass: "mobile-image",
-                            attrs: { src: mobile.image_path }
+                            attrs: { src: mobile.image }
                           }),
                           _vm._v(" "),
                           _c("div", { staticClass: "mobile-description" }, [
