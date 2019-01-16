ffa31b5f22704b16dc6b7752d1f82b5aba591054
omar <omar.coder007@gmail.com>
2019-01-16 12:47:11 +0300

    Search Component

--- resources/assets/js/components/SearchMobileComponent.vue
+++ resources/assets/js/components/SearchMobileComponent.vue
@@ -10,7 +10,7 @@
                 <p class="text-center" v-if="!filtered_mobiles.length">{{t['No Mobiles found']}}</p>
                 <div class="mobile-container" v-for="mobile in filtered_mobiles ">
                     <a class="tile-container" :href="mobile.show_url">
-                        <img :src="mobile.image_path" class="mobile-image">
+                        <img :src="mobile.image" class="mobile-image">
                         <div class="mobile-description">
                             <p>{{mobile.name}}</p>
                             <p>{{mobile.price}} $</p>
@@ -23,7 +23,7 @@
                 <span>{{t['Latest Visited']}}</span>
                 <div class="mobile-container" v-for="mobile in visited_mobiles">
                     <a  class="tile-container" :href="mobile.show_url">
-                        <img :src="mobile.image_path" class="mobile-image">
+                        <img :src="mobile.image" class="mobile-image">
                         <div class="mobile-description">
                             <p>{{mobile.name}}</p>
                             <p>{{mobile.price}} $</p>
